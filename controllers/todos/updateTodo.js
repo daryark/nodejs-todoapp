41779f6e72a768acbec18dc979ed42d0e49738f6
Dasha Yarkovska <yarkovska11@gmail.com>
2023-03-16 14:54:42 +0200

    little fix

--- controllers/todos/updateTodo.js
+++ controllers/todos/updateTodo.js
@@ -1,19 +1,15 @@
 const todoOperations = require("../../db/todo-func");
 
 const updateTodo = async (req, res) => {
-	try {
-		const { id } = req.params;
-		const result = await todoOperations.updateTodo(id, req.body);
-		res.json({
-			status: "success",
-			code: 200,
-			data: {
-				result,
-			},
-		});
-	} catch (error) {
-		next(error);
-	}
+	const { id } = req.params;
+	const result = await todoOperations.updateTodo(id, req.body);
+	res.json({
+		status: "success",
+		code: 200,
+		data: {
+			result,
+		},
+	});
 };
 
 module.exports = updateTodo;
